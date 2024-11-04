e7ea656b0967f71e8d44daa61b16103d2b0b7416
DanAth95 <daniel_atherton@aol.co.uk>
2024-11-04 15:30:01 +0000

    user can delete their own comments

--- src/components/SingleArticle/CommentCard.jsx
+++ src/components/SingleArticle/CommentCard.jsx
@@ -1,7 +1,36 @@
-export default function CommentCard({ comment }) {
+import { useContext, useState } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import axios from "axios";
+
+export default function CommentCard({ comment, commentList, setCommentList }) {
   const date = `${new Date(comment.created_at).getDate()}/${
     new Date(comment.created_at).getMonth() + 1
   }/${new Date(comment.created_at).getFullYear()}`;
+
+  const [error, setError] = useState("");
+  const [deleting, setDeleting] = useState(false);
+
+  const { user } = useContext(UserContext);
+
+  function handleClick() {
+    setDeleting(true);
+    setCommentList(
+      commentList.filter((c) => {
+        return c.comment_id !== comment.comment_id;
+      })
+    );
+    axios
+      .delete(
+        `https://nc-news-z5fx.onrender.com/api/comments/${comment.comment_id}`
+      )
+      .then(() => {
+        setDeleting(false);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }
+
   return (
     <>
       <p>
@@ -9,6 +38,11 @@ export default function CommentCard({ comment }) {
       </p>
       <p>{comment.body}</p>
       <p>Votes: {comment.votes}</p>
+      {user.username === comment.author ? (
+        <button onClick={handleClick} disabled={deleting ? true : false}>
+          X
+        </button>
+      ) : null}
     </>
   );
 }
